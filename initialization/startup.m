3d9a056cd304bc95e0855864a0c8c1cc58473a6d
lanluo9 <ll357@duke.edu>
2020-01-22 14:56:15 -0500

    init and dsRGC plot functions

---
+++ initialization/startup.m
@@ -0,0 +1,3 @@
+javaaddpath('/Applications/Vision.app/Contents/Resources/Java/Vision.jar')
+addpath(genpath('/Users/katthanya/Documents/MATLAB/matlab/code'))
+addpath(genpath('/Users/katthanya/Documents/MATLAB/matlab/private/Lan/'))
