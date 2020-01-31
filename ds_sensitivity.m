16134dfb49bfc7c932bc15d587df7f37f9ab4915
lanluo9 <ll357@duke.edu>
2020-01-31 15:14:35 -0500

    sorted sensitivity plot

--- ds_sensitivity.m
+++ ds_sensitivity.m
@@ -2,7 +2,7 @@
 % later will import ds cell id list to achieve sequential plotting for
 % every single ds cell
 
-%% changeable dataset_num & ds_now
+%% load data w changeable dataset_num & ds_now
 clear
 clc
 
@@ -13,30 +13,12 @@ prefix_now = '/Volumes/dusom_fieldlab';
 datapath = append(prefix_now, '/lab/Experiments/Array/Analysis/2019-11-21-0/data0', dataset_num, '/data0', dataset_num);
 % datapath = '/Volumes/???/lab/Experiments/Array/Analysis/2019-11-21-0/data002/data002';
 
-%% load data
 datarun = load_data(datapath);
 datarun = load_neurons(datarun);
 datarun = load_params(datarun);
 datarun = load_ei(datarun, 'all', 'array_type', 519);
-% datarun.names.stimulus_path = append(prefix_now, '/lab/Experiments/Array/Analysis/2019-11-21-0/stimuli/s', dataset_num, '.txt');
-% datarun.names.stimulus_path = '/Volumes/All_Staff/lab/Experiments/Array/Analysis/2019-11-21-0/stimuli/s02.txt';
 
-%% process triggers and extract some stim params
-% trigger_set = round(datarun.triggers);
-% trig_inds = find(mod(trigger_set, 10) == 0);
-% user_defined_trigs = datarun.triggers(trig_inds);
-% datarun = load_stim(datarun, 'user_defined_trigger_set', trig_inds);
-% num_stim = length(datarun.stimulus.combinations);
-% grat_dirs = datarun.stimulus.params.DIRECTION;
-% grat_TPs = datarun.stimulus.params.TEMPORAL_PERIOD;
-% stim_dur = 8;
-% num_reps = datarun.stimulus.repetitions;
-% num_rgcs = length(datarun.cell_ids);
-
-% verify datarun.spikes
-% histogram(datarun.spikes{1,1}, 40)
-
-%% scaffold: single section of darkness. cell id = 469, index = 21
+%% chop data000 to sections
 gaps = round(diff(datarun.triggers));
 switch_flag = gaps ~= 2 & gaps ~= 4;
 switch_duration = [round(datarun.triggers(1)) - 300; gaps(switch_flag)];
@@ -56,9 +38,10 @@ flash_config = [0; 2.4;2.4;2.8; 2.2;2.4;2.2;2.8;2.4; 2.2;2.8;2.4;0; 2.8;4.2;4.8;
 nflash = [0; sections(2:12,3)./2; 0; sections(14,3)./2; sections(15:16,3)./4; 0];
 sections = [sections, ndf, flash_config, nflash];
 
-%%
+%% scaffold: single section of darkness. cell id = 469, index = 21
 cell_index = 35; % slave index! find by datarun.cell_ids. match master id to slave id to slave index
 spike_time = datarun.spikes{cell_index, 1};
+
 section_id = 10; % range 1-17
 section_now = [sections(section_id, 1), sections(section_id, 2)];
 
@@ -66,7 +49,6 @@ section_flag = spike_time >= section_now(1) & spike_time <= section_now(2);
 spike_time_section = spike_time(section_flag);
 spike_time_section = spike_time_section - section_now(1);
 
-%%
 rep_max = round(section_now(2) - section_now(1)) / 2;
 for rep = 1 : rep_max
     rep_flag = spike_time_section >= (rep-1)*2 & spike_time_section <= rep*2;
@@ -79,16 +61,57 @@ for rep = 1 : rep_max
     axis([-0.05 2.05 0 (rep_max + 1)])
 end
 
-%% scaffold: sanity check
-% ntrig_flash = 60;
-% nflash = [60];
-% gap_seq = [300];
-% for i = 2 : length(switch_index)
-%     ntrig_flash = ntrig_flash + switch_index(i) - switch_index(i-1) -1;
-%     nflash = [nflash, (switch_index(i) - switch_index(i-1) - 1)];
-%     gap_seq = [gap_seq, (datarun.triggers(switch_index(i)) - datarun.triggers(switch_index(i-1)))];
-% end
-% % ntrig_flash + length(switch_index) + 1 == length(datarun.triggers)
-% gap_seq = [gap_seq, (datarun.triggers(end) - gap_seq(end))];
-% section_seq = transpose(gap_seq) - switch_duration;
+%% plot all sections separately
+for section_i = 1 : size(sections, 1)
+    subplot( size(sections, 1), 1, section_i )
+    title(['NDF = ', num2str(sections(section_i, 4))])
+
+    section_id = section_i; 
+    section_now = [sections(section_id, 1), sections(section_id, 2)];
+
+    section_flag = spike_time >= section_now(1) & spike_time <= section_now(2);
+    spike_time_section = spike_time(section_flag);
+    spike_time_section = spike_time_section - section_now(1);
+
+    rep_max = round(section_now(2) - section_now(1)) / 2;
+    for rep = 1 : rep_max
+        rep_flag = spike_time_section >= (rep-1)*2 & spike_time_section <= rep*2;
+        spike_time_rep = spike_time_section(rep_flag);
+        spike_time_rep = spike_time_rep - (rep-1)*2;
+
+        rep_mark = rep * ones(length(spike_time_rep),1);
+        scatter(spike_time_rep, rep_mark, 50, 'filled')
+        hold on
+        axis([-0.05 2.05 0 (rep_max + 1)])
+    end
+end
+
+%% sort sections by NDF and flash_config
+sections(:,7) = (sections(:,4)*(-10) + sections(:,5));
+section_sort = sortrows(sections, 7);
+
+for section_i = 1 : 15
+    subplot( 15, 1, section_i )
+
+    section_id = section_i; 
+    section_now = [section_sort(section_id, 1), section_sort(section_id, 2)];
+
+    section_flag = spike_time >= section_now(1) & spike_time <= section_now(2);
+    spike_time_section = spike_time(section_flag);
+    spike_time_section = spike_time_section - section_now(1);
+
+    rep_max = round(section_now(2) - section_now(1)) / 2;
+    for rep = 1 : rep_max
+        rep_flag = spike_time_section >= (rep-1)*2 & spike_time_section <= rep*2;
+        spike_time_rep = spike_time_section(rep_flag);
+        spike_time_rep = spike_time_rep - (rep-1)*2;
+
+        rep_mark = rep * ones(length(spike_time_rep),1);
+        scatter(spike_time_rep, rep_mark, 50, 'filled')
+        hold on
+        axis([-0.05 2.05 0 (rep_max + 1)])
+    end
+%     title(['NDF = ', num2str(section_sort(section_i, 4))])
+    
+end
 
