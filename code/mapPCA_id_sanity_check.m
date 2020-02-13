5fdcef1fe5268fefb24c256db2dde8a8033549fc
lanluo9 <ll357@duke.edu>
2020-02-13 13:44:48 -0500

    mapPCA sanity check: after

--- code/mapPCA_id_sanity_check.m
+++ code/mapPCA_id_sanity_check.m
@@ -1,21 +1,45 @@
 clear
 clc
+% contamination < 0.1
+% spike rate > 0.01
 
-dataset_num = '02-sorted';
-% dataset_num = '02';
+strprovide = struct('load_neurons',1,'load_params',0,'load_ei',0);
 
-prefix_now = '/Volumes/dusom_fieldlab';
-% prefix_now = '/Volumes/All_Staff/';
-% prefix_now = '/Volumes/dusom_fieldlab/All_Staff/';
+datarun = load_data('/Volumes/dusom_fieldlab/lab/Experiments/Array/Analysis/2019-11-21-0/rerun_san/data002-sorted/data002-sorted',strprovide); 
+datarun_s = load_data('/Volumes/dusom_fieldlab/lab/Experiments/Array/Analysis/2019-11-21-0/rerun_san/data000-map/data000-map',strprovide); 
+id_kept = intersect(datarun.cell_ids, datarun_s.cell_ids);
 
-datapath = append(prefix_now, '/lab/Experiments/Array/Analysis/2019-11-21-0/rerun/data0', dataset_num, ...
-    '/data0', dataset_num); % rerun
-% datapath = '/Volumes/???/lab/Experiments/Array/Analysis/2019-11-21-0/data002/data002';
+ds_cells_002_sorted = load('ds_master_002_sorted_20200211.mat'); 
+% load('ds_cell_map_20200210.mat', 'ds_map_all'); 
 
-datarun = load_data(datapath);
-datarun = load_neurons(datarun);
-% datarun = load_params(datarun);
-% datarun = load_ei(datarun, 'all', 'array_type', 519);
-datarun.names.stimulus_path = append(prefix_now, '/lab/Experiments/Array/Analysis/2019-11-21-0/stimuli/s', dataset_num, '.txt');
-% datarun.names.stimulus_path = '/Volumes/All_Staff/lab/Experiments/Array/Analysis/2019-11-21-0/stimuli/s02.txt';
+ds_cell_ids = ds_cells_002_sorted.ds_cells(2,:);
+ds_kept = intersect(id_kept, ds_cell_ids);
+ds_lost = setdiff(ds_cell_ids,intersect(id_kept, ds_cell_ids));
 
+length(intersect(id_kept, ds_cell_ids))
+length(ds_cell_ids)
+
+% length(datarun.cell_ids)
+% length(datarun_s.cell_ids)
+% length(id_kept)
+
+
+%%
+clear
+% contamination < 100
+% spike rate > 0.00
+strprovide = struct('load_neurons',1,'load_params',0,'load_ei',0);
+
+datarun = load_data('/Volumes/dusom_fieldlab/lab/Experiments/Array/Analysis/2019-11-21-0/rerun_san/data002-sorted/data002-sorted',strprovide); 
+datarun_ss = load_data('/Volumes/dusom_fieldlab/lab/Experiments/Array/Analysis/2019-11-21-0/rerun/data000-map/data000-map',strprovide); 
+id_kept = intersect(datarun.cell_ids, datarun_ss.cell_ids);
+
+ds_cells_002_sorted = load('ds_master_002_sorted_20200211.mat'); 
+ds_cell_ids = ds_cells_002_sorted.ds_cells(2,:);
+intersect(id_kept, ds_cell_ids)
+length(intersect(id_kept, ds_cell_ids))
+length(ds_cell_ids)
+
+% length(datarun.cell_ids)
+% length(datarun_ss.cell_ids)
+% length(id_kept)
