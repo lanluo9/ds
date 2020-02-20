4686ecc4d0b8cb7d3d0262a7d1175e94aba26441
lanluo9 <ll357@duke.edu>
2020-02-20 11:51:29 -0500

    MVR STA initial version done

--- code/MVR_STA/MVR_STA.m
+++ code/MVR_STA/MVR_STA.m
@@ -1,5 +1,8 @@
 %% import data
 
+clc
+clear
+
 time_white = importdata('whitenoise600k500us5hz3_5mVSD.xlsx'); 
 time_seq = time_white(:,1);
 white = time_white(:,2);
@@ -10,7 +13,7 @@ event = event(~isnan(event)); % delete NaNs
 %% sanity checks
 
 white_mean = mean(white)
-white_var = std(white) 
+white_var = std(white)
 % figure; plot(white)
 
 if min(event) < min(time_seq) || max(event) > max(time_seq)
@@ -20,11 +23,28 @@ end
 
 %% event triggering segment of white noise
 
+time_step = 0.5; % voltage and white noise is recorded every 0.5 ms
 ets_len = 1000; % define event triggering segment length as 1000 ms
-ets_start = event - ets_len;
-ets_start_end = [ets_start, event];
+ets_start_end = [event - ets_len, event];
 
-ets = zeros(length(ets_start_end), ets_len/0.5);
+ets = zeros(length(ets_start_end), ets_len/time_step);
 for i = 1:length(ets_start_end)
-    ets(i, :) = white(white(:,1)==ets_start_end());
+    id1 = find(time_seq==ets_start_end(i,1)) + 1;
+    id2 = find(time_seq==ets_start_end(i,2));
+    ets(i, :) = white(id1:id2)'; % ets is a matrix whose every row is a white noise voltage seq preceding an event
 end
+
+%% event triggering average
+
+eta = mean(ets,1);
+time_axis = [(-1*ets_len+time_step) : time_step : 0];
+
+plot(time_axis, eta, 'LineWidth',2)
+hold on 
+line([-1000,0],[0 0],'Color','Black')
+
+title('Event triggering average')
+xlabel('Time (ms)')
+ylabel('Voltage (mV)')
+
+
