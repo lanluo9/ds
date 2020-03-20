a4789dc9c691088f9637d684aa43f7cc75a1d5e3
lanluo9 <ll357@duke.edu>
2020-03-20 13:59:41 -0400

    config matlab

---
+++ code/matlab fav/startup_path_setting_windows.m
@@ -0,0 +1,8 @@
+% addpath(genpath('C:\Users\Katherine\Desktop\matlab\code'))
+% addpath(genpath(':C:\Users\Katherine\Desktop\matlab\private\katt'))
+addpath(genpath('D:/RRR/Grad/Rotation/GF_lab/lab_Mac/ds'))
+
+% javaaddpath('C:\Users\Katherine\Desktop\matlab\vision.jar')
+
+javaaddpath('D:\RRR\Grad\Rotation\GF_lab\code')
+% javarmpath('C:\Users\Katherine\Desktop\matlab\vision.jar')
