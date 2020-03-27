7994d8e77b736f5774e01a2e56a818741e2dd060
lanluo9 <ll357@duke.edu>
2020-03-27 12:32:11 -0400

    just in case

---
+++ code/ds_ventral/ds_2AFC.m
@@ -0,0 +1,54 @@
+%% load data 
+clear
+clc
+
+dataset_num = '/data000-map-sorted'; 
+date_num = '2020-02-29-0';
+
+% prefix_now = '/Volumes/dusom_fieldlab';
+prefix_now = '/Volumes/All_Staff/';
+
+datapath = append(prefix_now, '/lab/Experiments/Array/Analysis/', date_num, dataset_num, dataset_num);
+datarun = load_data(datapath);
+datarun = load_neurons(datarun);
+datarun = load_params(datarun);
+% datarun = load_ei(datarun, 'all', 'array_type', 519);
+
+%% load ds cell 
+
+load('ds_cell_map_20200306.mat', 'ds_map_all');
+flag = find(ds_map_all(:,1)==0);
+ds_slave_id_mapPCA = ds_map_all(1:(flag(1)-1), 2); ismember(ds_slave_id_mapPCA, datarun.cell_ids)
+ds_slave_id_mapEI = ds_map_all((flag(1)+1):(flag(2)-1), 2);
+ds_slave_id_map2 = ds_map_all((flag(2)+1):end, 2); ds_slave_id_map2(ds_slave_id_map2 == 0) = [];
+ds_map_all
+
+%% chop data000 into sections
+
+gaps = round(diff(datarun.triggers));
+switch_flag = gaps ~= 2 & gaps ~= 4;
+tmp_triggers = datarun.triggers(switch_flag);
+switch_index = [];
+for i = 1 : length(tmp_triggers)
+    switch_index = [switch_index, find(datarun.triggers == tmp_triggers(i))];
+end
+section_end = [300; datarun.triggers(switch_index); datarun.duration];
+section_start = [0; datarun.triggers(1); datarun.triggers(switch_index + 1)];
+sections = [section_start, section_end, (section_end - section_start)];
+sections(end, 2:3) = [datarun.triggers(end)+4, datarun.triggers(end)+4 - sections(end, 1)];
+sections(end+1, :) = [datarun.triggers(end)+4, datarun.duration, datarun.duration - (datarun.triggers(end)+4)];
+
+ndf = [99; 5;5;5;5;5;5;5; 4;4;4;4;4; 3;3;3; 2; 99];
+flash_config = [0; 2.2;2.8;2.4;2.1;2.2;2.4;2.8; 2.2;2.4;2.8;2.2;2.1; 2.2;2.4;4.8; 4.2; 0];
+nflash = [0; sections(2:15,3)./2; sections(16:17,3)./4; 0];
+sections = [sections, ndf, flash_config, nflash];
+
+sections(:,7) = (sections(:,4)*(-10) + sections(:,5));
+section_sort = sortrows(sections, 7);
+marker = unique(section_sort(:,7));
+marker_seq = section_sort(:,7);
+
+%% merge sections w same flash intensity. cuts off 2-4s
+
+slave_ds_id_all = unique(ds_map_all(:,2)); 
+slave_ds_id_all(slave_ds_id_all == 0) = [];
