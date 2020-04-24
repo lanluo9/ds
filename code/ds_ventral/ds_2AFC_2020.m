2a3a4eea6ccd7b46cee740d6ee369deb4150c6a4
lanluo9 <ll357@duke.edu>
2020-04-24 16:00:14 -0400

    fixed. before testing discriminant for noisy cells

--- code/ds_ventral/ds_2AFC_2020.m
+++ code/ds_ventral/ds_2AFC_2020.m
@@ -151,8 +151,8 @@ for c = 1 : length(ds_slave_normal)
                             trial_len*(order_post(t)-ntrial(fid_seq(1)))+section_idx(fid_seq(2),1));
                     end
                 elseif scale == 2 % BUG in dim
-                    trial_flash_post = binned(trial_len*(order_post(t)-1)+section_idx(fid_seq(1),1) - trial_len/2 + 1 : ...
-                            trial_len*(order_post(t)-1)+section_idx(fid_seq(1),1) );
+                    trial_flash_post = binned(trial_len*order_post(t)+section_idx(fid_seq(1),1) - trial_len/2 + 1 : ...
+                            trial_len*order_post(t)+section_idx(fid_seq(1),1));
                 end
                 other_flash_post = sum_flash_post - trial_flash_post;
                 mean_flash_post = other_flash_post ./ (length(trial_num_post) - 1) - mean_all .* length(trial_num_post) ./ (length(trial_num_post) - 1);
