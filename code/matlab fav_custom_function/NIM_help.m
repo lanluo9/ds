3cccafaca8966dbb44db9b4ae63dc499efd8d1dc
circuit <circuit@brahms.dhe.duke.edu>
2020-05-19 18:41:02 -0400

    wrote matlab favorite command NIM help

---
+++ code/matlab fav_custom_function/NIM_help.m
@@ -0,0 +1,9 @@
+currentEditor = matlab.desktop.editor.getActive;
+originalSelection = currentEditor.Selection;
+
+prefix = 'help NIM.';
+function_name = currentEditor.SelectedText;
+disp([prefix function_name])
+
+call_help = [prefix function_name];
+eval(call_help);
