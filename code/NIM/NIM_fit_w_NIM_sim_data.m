2ce482e072f2c02dbd1db102b99ddc956e8f838d
lanluo9 <ll357@duke.edu>
2020-06-01 18:53:35 -0400

    just in case bc matlab crashed again

--- code/NIM/NIM_fit_w_NIM_sim_data.m
+++ code/NIM/NIM_fit_w_NIM_sim_data.m
@@ -27,36 +27,36 @@ Robs_test = Robs(XVi);
 
 %%
 color = prism(size(pred_rates,2));
-bgn = 10;
-fin = 60;
+bgn = 300;
+fin = 500;
 
 figure('units','normalized','outerposition',[0 0 1 1])
 for i = 1:size(pred_rates,2)
-    pred_rate_now = pred_rates(:,i);
-    plot_pred = plot(bgn:fin-2, pred_rate_now(bgn : 5 : 5*(fin-bgn)), 'Color', color(i,:), 'LineWidth', 1)
+    pred_rate_now = pred_rates(:, i);
+    plot_pred = plot(pred_rate_now(bgn:fin), 'Color', color(i,:), 'LineWidth', 1);
     plot_pred.Color(4) = 0.7; % alpha transparency
     hold on
 end
-plot(bgn:fin, Robs_5(bgn:fin), 'k', 'LineWidth', 2)
+plot_spk = plot(Robs_test(bgn:fin), 'k', 'LineWidth', 2);
+plot_spk.Color(4) = 0.4; 
 legend({'0','S','1', '1S', '2', 'robs'}, 'Location','northeast')
 legend('boxoff')
 
 %%
 % R2_part = 1 - mean( (spk_per_bin(bgn:fin)' - pred_rates(bgn:fin,4)) .^2) / var(spk_per_bin(bgn:fin))
 for i = 1:5
-    R2(i) = 1 - mean( (Robs_5' - pred_rates(:,i)) .^2) / var(Robs_5)
+    R2(i) = 1 - mean( (Robs_test' - pred_rates(:,i)) .^2) / var(Robs_test);
 end
-
+R2
 % plot(R2)
 sum(R2>0) / size(R2,1) / size(R2,2) % model display seems reasonable though, should be due to buggy eval?
 
-%% generate fake spike train from model
-r_array = poissrnd(20,[1000 1]);
-mean(r_array)
-
-%%
-cell_id = 63;
-save(['perf-test-' num2str(cell_id) '.mat'], 'fit0', '-v7.3')
+% %% generate fake spike train from model
+% r_array = poissrnd(20,[1000 1]);
+% mean(r_array)
+% 
+% cell_id = 63;
+% save(['perf-test-' num2str(cell_id) '.mat'], 'fit0', '-v7.3')
 
 
 
