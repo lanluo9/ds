281949ff55b4b2fafba578d4042230ef09a7d8ef
lanluo9 <ll357@duke.edu>
2020-06-04 08:42:15 -0400

    trying

--- code/NIM/LNP_2018.m
+++ code/NIM/LNP_2018.m
@@ -86,136 +86,135 @@ Robs = NIM.Spks2Robs(spikes, binsize, NT );
 
 
 %% round 1 fit
-% disp('Fitting GLM: 1 excitatory filter')
-% mod_signs = [1]; % determines whether input is exc or sup (doesn't matter in the linear case)
-% NL_types = {'lin'}; % define subunit as linear 
-% 
-% % New object-based definition of model (note regularization can be set here)
-% fit0 = NIM( params_stim, NL_types, mod_signs, 'd2xt', 100 ); 
-% % also using spatial regularization too -- can do seprately or as one
-% 
-% % Tweak optimization parameters to doesn't creep around the minimum forever (necessary for 
-% % some reason with this data)
-% optim_params.maxIter = 400;
-% 
-% % Fit stimulus filters
-% fit0 = fit0.fit_filters( Robs, Xstim, Ui, 'optim_params', optim_params );
-% 
-% % find optimal regularization using nested cross-val (note this can take a while)
-% % fit_test = fit0.reg_path( Robs, Xstim, Ui, XVi, 'lambdaID', 'd2xt', 'optim_params', optim_params );
-% % Subunit 1: Best reg = 600.00 for reduction in space mov; 400 in time; 200
-% % for spatial RF
-% % this finds optimal value of XX
-% 
-% % look at filter
-% k0 = reshape(fit0.subunits(1).filtK, [nLags, NY * NX]);
-% [mval,bestlag] = max(max(abs(k0)')); bestlag 
-% 
-% 
-% figure(2); clf;
-% [U, S, V] = svd(k0);
-% subplot(1,2,1)
-% plot(U(:,1))
-% axis tight
-% axis square
-% subplot(1,2,2); colormap gray
-% imagesc(reshape(V(:,1), NY, NX))
-% axis square
-% % set(gcf, 'Position', get(0, 'Screensize'));
-% % saveas(gcf, ['k0_filter-' num2str(cell_id) '.png'])
-% close
-% 
-% % %% round 2 fit NIM
-% mod_signs = [1 -1]; % both inputs are excitatory. (+1 for excitatory, -1 for suppressive)
-% NL_types = {'rectlin','rectlin'}; % name-change
-% 
-% % Try spike history term
-% fitS = fit0.init_spkhist(16,'doubling_time',4, 'negcon'); % I don't believe in positive spk-history
-% fitS = fitS.fit_filters( Robs, Xstim, Ui, 'fit_offsets', 1, 'optim_params', optim_params );
-% 
-% % Always best to seed inhibition with different possibilities: random will often get stuck in local minima
-% fit1 = fit0;
-% fit1.subunits(1).NLtype='rectlin'; % note new name (from threshlin)
-% fit1.subunits(2) = fit1.subunits(1); % make second subunit like the first
-% fit1.subunits(2).weight = -1; % make suppressive
-% kshift = NIM.shift_mat_zpad(k0,4,1); % shift filter to be delayed
-% fit1.subunits(2).filtK = kshift(:); % flatten
-% 
-% fit1 = fit1.fit_filters( Robs, Xstim, Ui, 'fit_offsets', 1, 'optim_params', optim_params );
-% %fit1 = fit1.reg_path( Robs, Xstim, Ui, XVi, 'lambdaID', 'd2xt', 'fit_offsets', 1, 'optim_params', optim_params );
-% 
-% % Fit with spike-history term
-% fit1S = fit1.init_spkhist(16,'doubling_time',4, 'negcon');
-% fit1S = fit1S.fit_filters( Robs, Xstim, Ui, 'fit_offsets', 1, 'optim_params', optim_params );
-% % spike history term helps
-% fit1S.display_model('Xstims', Xstim )
-% % set(gcf, 'Position', get(0, 'Screensize'));
-% % saveas(gcf, ['fit1S-' num2str(cell_id) '.png'])
-% close
-% % plot by itself
+disp('Fitting GLM: 1 excitatory filter')
+mod_signs = [1]; % determines whether input is exc or sup (doesn't matter in the linear case)
+NL_types = {'lin'}; % define subunit as linear 
+
+% New object-based definition of model (note regularization can be set here)
+fit0 = NIM( params_stim, NL_types, mod_signs, 'd2xt', 100 ); 
+% also using spatial regularization too -- can do seprately or as one
+
+% Tweak optimization parameters to doesn't creep around the minimum forever (necessary for 
+% some reason with this data)
+optim_params.maxIter = 400;
+
+% Fit stimulus filters
+fit0 = fit0.fit_filters( Robs, Xstim, Ui, 'optim_params', optim_params );
+
+% find optimal regularization using nested cross-val (note this can take a while)
+% fit_test = fit0.reg_path( Robs, Xstim, Ui, XVi, 'lambdaID', 'd2xt', 'optim_params', optim_params );
+% Subunit 1: Best reg = 600.00 for reduction in space mov; 400 in time; 200
+% for spatial RF
+% this finds optimal value of XX
+
+% look at filter
+k0 = reshape(fit0.subunits(1).filtK, [nLags, NY * NX]);
+[mval,bestlag] = max(max(abs(k0)')); bestlag 
+
+
+figure(2); clf;
+[U, S, V] = svd(k0);
+subplot(1,2,1)
+plot(U(:,1))
+axis tight
+axis square
+subplot(1,2,2); colormap gray
+imagesc(reshape(V(:,1), NY, NX))
+axis square
+set(gcf, 'Position', get(0, 'Screensize'));
+saveas(gcf, ['k0_filter-' num2str(cell_id) '.png'])
+close
+
+% %% round 2 fit NIM
+mod_signs = [1 -1]; % both inputs are excitatory. (+1 for excitatory, -1 for suppressive)
+NL_types = {'rectlin','rectlin'}; % name-change
+
+% Try spike history term
+fitS = fit0.init_spkhist(16,'doubling_time',4, 'negcon'); % I don't believe in positive spk-history
+fitS = fitS.fit_filters( Robs, Xstim, Ui, 'fit_offsets', 1, 'optim_params', optim_params );
+
+% Always best to seed inhibition with different possibilities: random will often get stuck in local minima
+fit1 = fit0;
+fit1.subunits(1).NLtype='rectlin'; % note new name (from threshlin)
+fit1.subunits(2) = fit1.subunits(1); % make second subunit like the first
+fit1.subunits(2).weight = -1; % make suppressive
+kshift = NIM.shift_mat_zpad(k0,4,1); % shift filter to be delayed
+fit1.subunits(2).filtK = kshift(:); % flatten
+
+fit1 = fit1.fit_filters( Robs, Xstim, Ui, 'fit_offsets', 1, 'optim_params', optim_params );
+%fit1 = fit1.reg_path( Robs, Xstim, Ui, XVi, 'lambdaID', 'd2xt', 'fit_offsets', 1, 'optim_params', optim_params );
+
+% Fit with spike-history term
+fit1S = fit1.init_spkhist(16,'doubling_time',4, 'negcon');
+fit1S = fit1S.fit_filters( Robs, Xstim, Ui, 'fit_offsets', 1, 'optim_params', optim_params );
+% spike history term helps
+fit1S.display_model('Xstims', Xstim )
+set(gcf, 'Position', get(0, 'Screensize'));
+saveas(gcf, ['fit1S-' num2str(cell_id) '.png'])
+close
+% plot by itself
 % plot(fit1S.spk_hist.bin_edges(2:end), fit1S.spk_hist.coefs)
 % close
-% 
-% 
-% % new model display
-% fit1.display_model('Xstims',Xstim)
-% % set(gcf, 'Position', get(0, 'Screensize'));
-% % saveas(gcf, ['fit1-' num2str(cell_id) '.png'])
-% close
-% 
-% % can also plot RFs separately
-% k1 = reshape(fit1.subunits(1).filtK, [nLags, NY*NX]);
-% k2 = reshape(fit1.subunits(2).filtK, [nLags, NY*NX]);
-% [~,best_pixel] = max(max(abs(k1)));
-% [mk1,blat1] = max(max(abs(k1')));
-% [mk2,blat2] = max(max(abs(k2')));
-% 
-% figure
-% subplot(1,3,1); hold on
-% plot(k1(:,best_pixel)/std(k1(:,best_pixel)),'b')
-% plot(k2(:,best_pixel)/std(k2(:,best_pixel)),'r')
-% axis tight
-% % Note that there is really only a delay between the filters of ~4 ms, which means that they will have a hard
-% % time explaining differences in timing at the resolution of the PSTH (where event width >>4 ms)
-% subplot(1,3,2); colormap gray
-% imagesc(reshape(k1(blat1,:),NY,NX),[-1 1]*mk1)
-% axis square
-% subplot(1,3,3); colormap gray
-% imagesc(reshape(k2(blat2,:),NY,NX), [-1 1]*mk2)
-% axis square
-% % set(gcf, 'Position', get(0, 'Screensize'));
-% % saveas(gcf, ['subunits-' num2str(cell_id) '.png'])
-% close
-% 
-% fit0nl = fit0.fit_spkNL( Robs, Xstim, Ui );
-% fit1nl = fit1.fit_spkNL( Robs, Xstim, Ui );
-% 
-% % fit upstream nonlinearity?
-% fit2 = fit1.init_nonpar_NLs( Xstim );
-% fit2 = fit2.fit_upstreamNLs( Robs, Xstim, Ui );
-% % this actually ends up looking rectlin, so not worth it
-% fit2.display_model('Xstims',Xstim)
-% % set(gcf, 'Position', get(0, 'Screensize'));
-% % saveas(gcf, ['fit2-' num2str(cell_id) '.png'])
-% close
-% 
-% % internal LLx (cross-validated likelihood, although was used for meta-params)
-% [LLs,~,~,fp] = fit0.eval_model(Robs, Xstim, XVi );
-% LLs(2) = fitS.eval_model(Robs, Xstim, XVi );
-% LLs(3) = fit1.eval_model(Robs, Xstim, XVi );
-% LLs(4) = fit1S.eval_model(Robs, Xstim, XVi );
-% LLs(5) = fit2.eval_model(Robs, Xstim, XVi ); % fit2 (w nonlin) is not better than fit1
-% LLs-fp.nullLL
-% 
-% % save Res2mod3.mat fit0 fit1
-% save('demo_adapt.mat', '-v7.3') % force save >2GB .mat
+
+
+% new model display
+fit1.display_model('Xstims',Xstim)
+set(gcf, 'Position', get(0, 'Screensize'));
+saveas(gcf, ['fit1-' num2str(cell_id) '.png'])
+close
+
+% can also plot RFs separately
+k1 = reshape(fit1.subunits(1).filtK, [nLags, NY*NX]);
+k2 = reshape(fit1.subunits(2).filtK, [nLags, NY*NX]);
+[~,best_pixel] = max(max(abs(k1)));
+[mk1,blat1] = max(max(abs(k1')));
+[mk2,blat2] = max(max(abs(k2')));
+
+figure
+subplot(1,3,1); hold on
+plot(k1(:,best_pixel)/std(k1(:,best_pixel)),'b')
+plot(k2(:,best_pixel)/std(k2(:,best_pixel)),'r')
+axis tight
+% Note that there is really only a delay between the filters of ~4 ms, which means that they will have a hard
+% time explaining differences in timing at the resolution of the PSTH (where event width >>4 ms)
+subplot(1,3,2); colormap gray
+imagesc(reshape(k1(blat1,:),NY,NX),[-1 1]*mk1)
+axis square
+subplot(1,3,3); colormap gray
+imagesc(reshape(k2(blat2,:),NY,NX), [-1 1]*mk2)
+axis square
+set(gcf, 'Position', get(0, 'Screensize'));
+saveas(gcf, ['subunits-' num2str(cell_id) '.png'])
+close
+
+fit0nl = fit0.fit_spkNL( Robs, Xstim, Ui );
+fit1nl = fit1.fit_spkNL( Robs, Xstim, Ui );
+
+% fit upstream nonlinearity?
+fit2 = fit1.init_nonpar_NLs( Xstim );
+fit2 = fit2.fit_upstreamNLs( Robs, Xstim, Ui );
+% this actually ends up looking rectlin, so not worth it
+fit2.display_model('Xstims',Xstim)
+set(gcf, 'Position', get(0, 'Screensize'));
+saveas(gcf, ['fit2-' num2str(cell_id) '.png'])
+close
+
+% internal LLx (cross-validated likelihood, although was used for meta-params)
+[LLs,~,~,fp] = fit0.eval_model(Robs, Xstim, XVi );
+LLs(2) = fitS.eval_model(Robs, Xstim, XVi );
+LLs(3) = fit1.eval_model(Robs, Xstim, XVi );
+LLs(4) = fit1S.eval_model(Robs, Xstim, XVi );
+LLs(5) = fit2.eval_model(Robs, Xstim, XVi ); % fit2 (w nonlin) is not better than fit1
+LLs-fp.nullLL
+
+% save Res2mod3.mat fit0 fit1
+% save('demo_adapt_63_13.mat', '-v7.3') % force save >2GB .mat
+save('before_refit_63_13.mat', '-regexp', 'fit*')
 
 %%
-% save('model_prep_63.mat', '-v7.3') 
-
 % load perf-1578.mat
 % load demo_adapt.mat
-load before-refit-63.mat
+% load before-refit-63.mat % model vs Xstim spatial mismatch
 
 [~,pred_rate0,~,~] = fit0.eval_model(Robs, Xstim, XVi );
 [~,pred_rateS,~,~] = fitS.eval_model(Robs, Xstim, XVi );
